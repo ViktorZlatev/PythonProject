905265db117a08ae6118f5701c827d40d3c9a4a8
Kaloyan Anastasov <kaloanas07@gmail.com>
2023-05-10 21:04:26 +0300

    image and video nudity detection added

--- website/mychatapp/views.py
+++ website/mychatapp/views.py
@@ -20,7 +20,7 @@ y = df[df.columns[2:]].values
 MAX_FEATURES = 200000
 vectorizer = TextVectorization(max_tokens=MAX_FEATURES, output_sequence_length=1800, output_mode='int')
 vectorizer.adapt(X.values)
-TOXICITY_FLAG = 0.65
+
 
 
 # Create your views here.
